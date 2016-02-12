95bffd47d32ffdc31f3df7b27c4d3874d5f093e3
Freja Nordsiek <fnordsie@gmail.com>
2016-02-12 20:44:11 +0100

    Updated the copyright years in the source files.

--- GeminiMotorDrive/utilities.py
+++ GeminiMotorDrive/utilities.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2015 Freja Nordsiek
+# Copyright 2014-2016 Freja Nordsiek
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
